61efcfd421404d9620f54e7f77b3c80a9ca1e928
quocdatmkt <quocdatmkt@gmail.com>
2024-02-25 10:39:26 +0700

    Adding Port for telegram port

--- apps/site/app/Http/Controllers/Shop/ChapterController.php
+++ apps/site/app/Http/Controllers/Shop/ChapterController.php
@@ -211,7 +211,7 @@ class ChapterController
         if($create_new == 2) {
             $client = new Client();
 
-            $response = $client->post('http://103.116.104.183/chapter/create-new', [
+            $response = $client->post('http://103.116.104.183:3002/chapter/create-new', [
                 'json' => [
                     'chapter' => $chapter
                 ]
@@ -244,7 +244,7 @@ class ChapterController
     }
 
     public function getRemoteData($telegram_id, $storyId, $id) {
-        $url = "http://103.116.104.183/chapter/file-by-id/{$telegram_id}/storyId/{$storyId}/chapterId/${id}";
+        $url = "http://103.116.104.183:3002/chapter/file-by-id/{$telegram_id}/storyId/{$storyId}/chapterId/${id}";
         $client = new Client();
         $response = $client->get($url);
 
@@ -253,7 +253,7 @@ class ChapterController
     }
 
     public function getRemoteVipData($storyId, $id) {
-        $url = "http://103.116.104.183/chapter/vip/storyId/{$storyId}/id/{$id}";
+        $url = "http://103.116.104.183:3002/chapter/vip/storyId/{$storyId}/id/{$id}";
 
         $client = new Client();
         $response = $client->get($url);
