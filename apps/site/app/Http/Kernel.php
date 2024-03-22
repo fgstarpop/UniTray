e29e4580768a009f0f43c46b6b628436c6ab3909
quocdatmkt <quocdatmkt@gmail.com>
2024-03-22 17:14:47 +0700

    [BE] Adding ratelimit for web

--- apps/site/app/Http/Kernel.php
+++ apps/site/app/Http/Kernel.php
@@ -32,6 +32,7 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
+            'throttle:20,1',
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
