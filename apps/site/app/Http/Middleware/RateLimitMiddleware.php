efe85af2663ff6e20e2f61e0dc587d551a8c7f54
barryle <lmkhang89@gmail.com>
2024-03-22 17:07:07 +0700

    [BE] Adding ratelimit is 20

---
+++ apps/site/app/Http/Middleware/RateLimitMiddleware.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Cache\RateLimiter;
+use Symfony\Component\HttpFoundation\Response;
+
+class RateLimitMiddleware extends Middleware
+{
+    protected $limiter;
+
+    public function __construct(RateLimiter $limiter)
+    {
+        $this->limiter = $limiter;
+    }
+
+    public function handle($request, Closure $next)
+    {
+        $key = $this->resolveRequestSignature($request);
+
+        if ($this->limiter->tooManyAttempts($key, 20)) {
+            return new Response('Too Many Requests', 429);
+        }
+
+        $this->limiter->hit($key, 1);
+
+        $response = $next($request);
+
+        return $response;
+    }
+
+    protected function resolveRequestSignature($request)
+    {
+        return sha1($request->method() . '|' . $request->server('SERVER_NAME') . '|' . $request->ip());
+    }
+}
