9e1a438ba7853ef92766fa538637d7379f6c1ff5
fgstarpop <quocdatmkt@gmail.com>
2024-04-13 22:34:44 +0700

    Đổi giá gói VIP
    Đổi giá gói VIP

--- apps/site/app/Http/Controllers/User/AccountController.php
+++ apps/site/app/Http/Controllers/User/AccountController.php
@@ -368,7 +368,7 @@ class AccountController
 
     public function upgradeVip()
     {
-        $vipPrice = 200000;
+        $vipPrice = 100000;
         if(!Auth::guard('web')->check()){
             flash()->error('Bạn cần đăng nhập để sử dụng tính năng này');
             return redirect()->route('login');
