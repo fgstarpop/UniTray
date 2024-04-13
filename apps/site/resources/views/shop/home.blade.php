4c4a07632b240312e68f6c7054ddc3e6c79d3945
fgstarpop <quocdatmkt@gmail.com>
2024-04-13 22:16:23 +0700

    Thêm code ads vipads.cc
    Thêm code ads vipads.cc

--- apps/site/resources/views/shop/home.blade.php
+++ apps/site/resources/views/shop/home.blade.php
@@ -52,7 +52,9 @@
         </div>
     </div>
     @if((new \Jenssegers\Agent\Agent())->isMobile())
-
+    @if((currentUser() && currentUser()->user_vip == 0) || !currentUser())
+<script src="https://www.vipads.live/vn/c-1734-25.js"></script>
+@endif
         <section class="content container mt-2 search-section" style="min-height: unset">
             <div class="col-12">
                 <div class="noti-block p-3 mb-3 border-danger border-3" style="border-style: solid; background-color: #FFF8B7;">
