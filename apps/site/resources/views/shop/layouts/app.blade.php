7061d0655756d8952e4ab3c455b88a1f6270e0f9
fgstarpop <quocdatmkt@gmail.com>
2024-04-21 21:29:31 +0700

    Thêm ads google

--- apps/site/resources/views/shop/layouts/app.blade.php
+++ apps/site/resources/views/shop/layouts/app.blade.php
@@ -4,6 +4,8 @@
     <base href="{{ asset('') }}"/>
     <meta charset="utf-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3169328496754307"
+     crossorigin="anonymous"></script>
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
