03cfa4d4b9eda51d6b5328d7fbdffe1e9cb61050
fgstarpop <quocdatmkt@gmail.com>
2024-05-01 00:22:43 +0700

    test màu

--- apps/site/resources/views/shop/layouts/header-mobile.blade.php
+++ apps/site/resources/views/shop/layouts/header-mobile.blade.php
@@ -16,7 +16,7 @@
 
     <div class="header header-mobile">
         <div class="back-gr-mobile" style="height: 100%;overflow: hidden;"><img
-                src="{{ asset('frontend/images/mobile/bgm2.png') }}"
+                src="{{ asset('frontend/images/mobile/bgm3.png') }}"
                 alt="{{ setting('store_name', 'Giangthe.online') }}"></div>
         <div class="container">
             <div class="row" style="display: flex; align-items: center;">
