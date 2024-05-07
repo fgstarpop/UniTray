399cfed2d8037dcec3dabcc29397de9b0faba4d2
fgstarpop <quocdatmkt@gmail.com>
2024-05-07 08:52:05 +0700

    Chỉnh giá chương vip

--- apps/site/config/vipfaloo.php
+++ apps/site/config/vipfaloo.php
@@ -8,9 +8,9 @@ return [
 
     "priceworduservip" => 0, // giá tiền cho mỗi chương của user vip giáng thế khi mua chương đã có trong db
 
-    "pricewordolduser" => 100, // giá tiền cho mỗi chương của user mua lại những chương đã có trong db
+    "pricewordolduser" => 130, // giá tiền cho mỗi chương của user mua lại những chương đã có trong db
 
-    "percentprofit" => 1, // phần trăm lơi nhuận khi mua chương chia cho user mua đầu tiên
+    "percentprofit" => 0.8, // phần trăm lơi nhuận khi mua chương chia cho user mua đầu tiên
 
     "percentdonate" => 0,// phần trăm donate chia cho user mua chương đầu tiên
 
