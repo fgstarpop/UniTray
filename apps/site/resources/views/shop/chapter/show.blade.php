13fd5f071efd1ab6e189f17d2d79ee0f64f08386
fgstarpop <quocdatmkt@gmail.com>
2024-05-10 00:13:02 +0700

    Test ads

--- apps/site/resources/views/shop/chapter/show.blade.php
+++ apps/site/resources/views/shop/chapter/show.blade.php
@@ -63,6 +63,15 @@
     @endphp
     {{-- Desktop --}}
     @if ((new \Jenssegers\Agent\Agent())->isDesktop())
+    @if ((currentUser() && currentUser()->user_vip == 0) || !currentUser())
+    <hr style="height: 16px; background: #eae4d3; margin-top: 0px;">
+    <div style="font-size: 14px; color: #545454; text-align: center;">
+        <h9 class="title">
+            <strong> --- Click quảng cáo ở phút 0-5, 30-35 mỗi giờ để nhận quà---</strong>
+              @include('shop.layouts.partials.ads')
+    </div>
+    <hr style="height: 16px; background: #eae4d3; margin-top: 0px;">
+@endif
         <style>
             a {
                 color: #191919 !important;
--- apps/site/resources/views/shop/layouts/partials/ads.blade.php
+++ apps/site/resources/views/shop/layouts/partials/ads.blade.php
@@ -23,10 +23,10 @@
     @endphp
     <div class="ads-google-section position-relative">
         <div class="position-absolute {{ $canEarn ? '' : 'hidden'}}" id="earn-fade" style="width:100%;height:100%;top:0;left:0;z-index:999"></div>
-        <iframe data-aa='2323873' src='//ad.a-ads.com/2323873?size=320x100' style='width:320px; height:100px; border:0px; padding:0; overflow:hidden; background-color: transparent;'></iframe>
+        <div id='frame' style='width:320px;'><iframe data-aa='2323874' src='//ad.a-ads.com/2323874?size=320x100' style='width:320px; height:100px; border:0px; padding:0; overflow:hidden; background-color: transparent;'></iframe><a style='display: block; text-align: right; font-size: 12px' id='preview-link' href='https://a-ads.com/?partner=2323874'>Advertise here</a></div>
     </div>
         @elseif(empty(currentUser()))
-        <iframe data-aa='2323873' src='//ad.a-ads.com/2323873?size=320x100' style='width:320px; height:100px; border:0px; padding:0; overflow:hidden; background-color: transparent;'></iframe>
+        <div id='frame' style='width:320px;'><iframe data-aa='2323874' src='//ad.a-ads.com/2323874?size=320x100' style='width:320px; height:100px; border:0px; padding:0; overflow:hidden; background-color: transparent;'></iframe><a style='display: block; text-align: right; font-size: 12px' id='preview-link' href='https://a-ads.com/?partner=2323874'>Advertise here</a></div>
         @endif
 
 
