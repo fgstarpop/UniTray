a8e83d3775e02e2440d67de1914ef35493638490
fgstarpop <quocdatmkt@gmail.com>
2024-09-27 01:04:44 +0700

    Update vipfaloo.php

--- apps/site/config/vipfaloo.php
+++ apps/site/config/vipfaloo.php
@@ -17,8 +17,8 @@ return [
     "minword" => 1000, // số chữ tối thiểu để tính tiền
 
     "method" => 0, // 0: tính theo 1000 chữ, 1: tính theo giá vip của faloo
-    "proxy" => "socks5://oefu0259:FTAbmt6530@157.66.163.80:34921",
+    "proxy" => "socks5://ohuy0895:QJSqlt7469@160.30.91.3:43541",
     //cookie đã đăng nhập của faloo
-    "cookie" => "KeenFire=UMID=34307370&UserID=fgstarpopp&Pwd=28a78f79ce2d8b6aec7daa4ec03d0342&Identity=web45562.1012424284&PhotoID=0&NickName=fgstarpopp; UU12345678=uuc=133591887448422283345958489"
+    "cookie" => "KeenFire=UMID=34307370&UserID=fgstarpopp&Pwd=281a826d30bf4906a5076d4ffa23090e&Identity=web45562.2606329226&PhotoID=0&NickName=fgstarpopp; UU12345678=uuc=133591887448422283345958489"
     // "cookie" => "KeenFire=UMID=34092005&UserID=hoanganh97&Pwd=74639c9632ac61ab0292f3ee2d30ceeb&Identity=web45394.1252113685&PhotoID=0&NickName=hoanganh97; UU12345678=uuc=133573096409661720189114593"
 ];
