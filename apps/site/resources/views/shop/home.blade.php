7dd36d97d6830266959fa9495f4e565dcfda7935
fgstarpop <quocdatmkt@gmail.com>
2024-09-27 09:44:17 +0700

    Update home.blade.php

--- apps/site/resources/views/shop/home.blade.php
+++ apps/site/resources/views/shop/home.blade.php
@@ -239,6 +239,7 @@
                                 @endforeach
                             </ul>--}}
                         </div>
+
                         <div class="paginate">
 
                         </div>
